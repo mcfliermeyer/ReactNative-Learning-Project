7f6991c7029cb879343654943332ccfc568cf9c6
Mark Meyer <mcfliermeyer@gmail.com>
2022-09-20 07:06:25 -0400

    Start contact form with styles.

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.nameContainer}>
+        <Text style={styles.nameLabel}>Name</Text>
+        <TextInput style={styles.nameInput} />
+      </View>
+      <Text>making a contact form for practice!</Text>
     </View>
   );
 }
@@ -13,8 +16,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  nameContainer: {
+    backgroundColor: "ghostwhite",
+    width: "80%",
+    height: "40%",
+  },
+  nameLabel: {
+    backgroundColor: "aqua"
   },
 });
