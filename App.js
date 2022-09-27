fdf159686c560443341619a7079694044d1d63b0
Mark Meyer <mcfliermeyer@gmail.com>
2022-09-27 07:04:46 -0400

    SafeAreaView and Android emulator setup.

--- App.js
+++ App.js
@@ -1,31 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.nameContainer}>
-        <Text style={styles.nameLabel}>Name</Text>
-        <TextInput style={styles.nameInput} />
-      </View>
-      <Text>making a contact form for practice!</Text>
+    <View style={{ backgroundColor: "red", flex: 1 }}>
+      <SafeAreaView style={styles.safeContainer}>
+        <View style={styles.container}>
+          <View style={styles.nameContainer}>
+            <Text style={styles.nameLabel}>Name</Text>
+            <TextInput style={styles.nameInput} />
+          </View>
+          <Text>making a contact form for practice!</Text>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "honeydew",
     alignItems: "center",
-    justifyContent: "center",
   },
   nameContainer: {
+    margin: 20,
     backgroundColor: "ghostwhite",
     width: "80%",
-    height: "40%",
+    height: 60,
+    justifyContent: "center",
   },
   nameLabel: {
-    backgroundColor: "aqua"
+    margin: 10,
+    padding: 10,
+    fontSize: 18,
+    backgroundColor: "aqua",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  nameInput: {
+    minWidth: "80%",
+    height: 60,
+    backgroundColor: "#a1e192",
   },
 });
